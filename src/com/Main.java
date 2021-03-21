b2b33eac92524963fa914a64b5315ec8478c482f
Marcelo <mgscosta@ice.ufjf.br>
2021-03-20 22:37:55 -0300

    add read file

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,9 +1,29 @@
 package com;
 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        Encoder en = new Encoder("bananabanabofana");
-        en.encode();
+
+
+        try {
+            String file = "string.txt";
+            Scanner scanner = new Scanner(new File("string.txt"));
+            String  word = null;
+            while(scanner.hasNext()) {
+               word = scanner.useDelimiter("\\A").next();
+            }
+            scanner.close();
+            Encoder en = new Encoder(word);
+            en.encode();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+
     }
 
 }
